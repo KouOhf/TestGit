ebb258b3ac8ad43c677e67fa72f6722e07daa4b2
PC_User <PC_User@DESKTOP-2456LPP>
2022-03-30 03:43:41 +0900

    リスト４完成

--- Todolist4/src/main/java/com/example/todolist/repository/TodoRepository.java
+++ Todolist4/src/main/java/com/example/todolist/repository/TodoRepository.java
@@ -1,5 +1,8 @@
 package com.example.todolist.repository;
 
+import java.sql.Date;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +12,18 @@ import com.example.todolist.entity.Todo;
 //JpaRepository<Todo, Integer> ・・・ 第一引数＝entityのクラス/第二引数＝「主キー(@ID)のクラス」
 //これにより、CRUDが一通り使える
 public interface TodoRepository extends JpaRepository<Todo, Integer>{
-
+	List<Todo> findByTitleLike(String title);
+	List<Todo> findByImportance(Integer importance);
+	List<Todo> findByUrgency(Integer urgency);
+	List<Todo> findByDeadlineBetweenOrderByDeadlineAsc(Date from, Date to);
+	List<Todo> findByDeadlineGreaterThanEqualOrderByDeadlineAsc(Date from);
+	List<Todo> findByDeadlineLessThanEqualOrderByDeadlineAsc(Date to);
+	List<Todo> findByDone(String done);
+	/*
+	 * 【命名規則に準じた抽象メソッドをリポジトリに宣言すると、SpringBootが自動実装する】
+	 *findBy～Like = その 文字( 列)を～に含む
+	 * findBy～Between～Asc　＝　（この場合）開始～終了の範囲内に期限が収まっている※Asc=昇順
+	 *  findBy～GreaterThanEqualOrderBy～Asc＝～以上の。SQL⇒『SELECT * FROM todo WHERE importance = ーー
+	 *  ーー＞引数 importanceの値 AND urgency = 引数 urgency の値』
+	 * */
 }
--- Todolist4/src/main/java/com/example/todolist/service/TodoService.java
+++ Todolist4/src/main/java/com/example/todolist/service/TodoService.java
@@ -2,17 +2,27 @@ package com.example.todolist.service;
 
 import java.time.DateTimeException;
 import java.time.LocalDate;
+import java.util.List;
 
 import org.springframework.stereotype.Service;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 
+import com.example.todolist.common.Utils;
+import com.example.todolist.entity.Todo;
 import com.example.todolist.form.TodoData;
 import com.example.todolist.form.TodoQuery;
+import com.example.todolist.repository.TodoRepository;
+
+import lombok.AllArgsConstructor;
 
 @Service
+@AllArgsConstructor
 public class TodoService {
-	public boolean isValid(TodoData todoData,BindingResult result) {
+	
+	private final TodoRepository todoRepository;
+	
+ 	public boolean isValid(TodoData todoData,BindingResult result) {
 		boolean ans = true;
 		//件名が全角スペースだけで構成されていたらエラー
 		String title = todoData.getTitle();
@@ -81,5 +91,38 @@ public class TodoService {
 		return ans;
 	}
 	
-	
+	public List<Todo> doQuery(TodoQuery todoQuery){
+		
+		List<Todo> todoList = null;
+		if(todoQuery.getTitle().length()>0) {
+			/*タイトルで検索*/
+			todoList = todoRepository.findByTitleLike("%" + todoQuery.getTitle() + "%");
+		} else if(todoQuery.getImportance() != null && todoQuery.getImportance() != -1) {
+			/*重要度で検索*/
+			todoList = todoRepository.findByImportance(todoQuery.getImportance());
+		} else if(todoQuery.getUrgency() != null && todoQuery.getUrgency() != -1) {
+			/*緊急度で検索*/
+			todoList = todoRepository.findByUrgency(todoQuery.getUrgency());
+		} else if(!todoQuery.getDeadlineFrom().equals("") && todoQuery.getDeadlineTo().equals("")) {
+			/*期限 : 開始～*/
+			todoList = todoRepository.findByDeadlineGreaterThanEqualOrderByDeadlineAsc
+					(Utils.str2date(todoQuery.getDeadlineFrom()));
+		} else if(todoQuery.getDeadlineFrom().equals("") && !todoQuery.getDeadlineTo().equals("")) {
+			/*期限 : ～終了*/
+			todoList = todoRepository.findByDeadlineLessThanEqualOrderByDeadlineAsc
+					(Utils.str2date(todoQuery.getDeadlineTo()));
+		} else if(!todoQuery.getDeadlineFrom().equals("") && !todoQuery.getDeadlineTo().equals("")) {
+			/*期限 : 開始～終了*/
+			todoList = todoRepository.findByDeadlineBetweenOrderByDeadlineAsc
+					(Utils.str2date(todoQuery.getDeadlineFrom()), Utils.str2date(todoQuery.getDeadlineTo()));
+		} else if(todoQuery.getDone() != null && todoQuery.getDone().equals("Y")) {
+			/*完了で検索*/
+			todoList = todoRepository.findByDone("Y");
+		} else {
+			/*入力条件がなければ全件検索*/
+			todoList = todoRepository.findAll();
+		}
+		
+		return todoList;
+	}
 }
